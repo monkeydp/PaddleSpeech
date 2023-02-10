c3627e568661713475760fdafb7f2173cd3c6923
iPotato <35134112+monkeydp@users.noreply.github.com>
2023-02-10 14:50:59 +0800

    SCTK 升级到 2.4.12

--- tools/extras/install_sclite.sh
+++ tools/extras/install_sclite.sh
@@ -3,8 +3,8 @@
 WGET="wget --no-check-certificate"
 
 # SCTK official repo does not have version tags. Here's the mapping:
-# # 2.4.9 = 659bc36; 2.4.10 = d914e1b; 2.4.11 = 20159b5.
-SCTK_GITHASH=20159b5
+# # 2.4.9 = 659bc36; 2.4.10 = d914e1b; 2.4.11 = 20159b5; 2.4.12 = 62aabfa
+SCTK_GITHASH=62aabfa
 SCTK_CXFLAGS="-w -march=native"
 CFLAGS="CFLAGS=${SCTK_CXFLAGS}"
 CXXFLAGS="CXXFLAGS=-std=c++11 ${SCTK_CXFLAGS}"
@@ -12,7 +12,7 @@ CXXFLAGS="CXXFLAGS=-std=c++11 ${SCTK_CXFLAGS}"
 MAKE=make
 
 
-${WGET} -nv -T 10 -t 3 -O sctk-${SCTK_GITHASH}.tar.gz  https://github.com/usnistgov/SCTK/archive/${SCTK_GITHASH}.tar.gz; 
+${WGET} -nv -T 10 -t 3 -O sctk-${SCTK_GITHASH}.tar.gz  https://github.com/usnistgov/SCTK/archive/${SCTK_GITHASH}.tar.gz;
 tar zxvf sctk-${SCTK_GITHASH}.tar.gz
 rm -rf sctk-${SCTK_GITHASH} sctk
 mv SCTK-${SCTK_GITHASH}* sctk-${SCTK_GITHASH}
